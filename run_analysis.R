77053e9bda2e256a62cc6d7832ade94660c531af
marjanmaz <mmazroue@stevens.edu>
2016-03-13 17:20:50 -0400

    Run Analysis uploaded

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+if(!file.exists("./data1")){dir.create("./data1")}
+fileUrl10 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl10,destfile="F:/pc/Getting and Cleaning Data/data1/Dataset.zip")
+unzip(zipfile="./data1/Dataset.zip",exdir="./data1")
+path <- file.path("./data1" , "UCI HAR Dataset")
+files<-list.files(path, recursive=TRUE)
+files
+dataActivityTest  <- read.table(file.path(path, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path, "train", "Y_train.txt"),header = FALSE)
+dataSubjectTrain <- read.table(file.path(path, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path, "test" , "subject_test.txt"),header = FALSE)
+dataFeaturesTest  <- read.table(file.path(path, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path, "train", "X_train.txt"),header = FALSE)
+str(dataActivityTest)
+str(dataActivityTrain)
+str(dataSubjectTrain)
+str(dataSubjectTest)
+str(dataFeaturesTest)
+str(dataFeaturesTrain)
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+str(Data)
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+head(Data$activity,30)
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
